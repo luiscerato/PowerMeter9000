6b4f13c2b283d86a1a2fd8459ad26b969325a76d
Luis Cerato <luiscerato@gmail.com>
2024-03-28 19:59:37 -0300

    Interfase web

--- data/index.js
+++ data/index.js
@@ -23,8 +23,30 @@ tableRows = [
     {name: "Promedio", short: "Avg", visible: true},
 ];
 
+
 function initUI() {
+    
+$.fn.gauge = function(opts) {
+    this.each(function() {
+      var $this = $(this),
+          data = $this.data();
+  
+      if (data.gauge) {
+        data.gauge.stop();
+        delete data.gauge;
+      }
+      if (opts !== false) {
+        data.gauge = new Gauge(this).setOptions(opts);
+      }
+    });
+    return this;
+  };
+  
+
+    $("#gauge1").gauge();
+
     createTable();
+
 
     board.getResponseRepeat("meter", meterOk, meterFail, "fast");
 
